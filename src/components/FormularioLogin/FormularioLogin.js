6f9303c8fb0d78dc9713815cef2fa594566f0d73
LuizHenriqueMarinho <luizhenriquemarinhofga@gmail.com>
2022-10-19 17:49:07 -0300

    exercicios a036 resolvidos

--- src/components/FormularioLogin/FormularioLogin.js
+++ src/components/FormularioLogin/FormularioLogin.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   Form,
   FormContainer,
@@ -9,18 +9,34 @@ import {
 
 const FormularioLogin = (props) => {
   const login = () => {
+    const globalObjetc = {
+      nome: props.nome,
+      foto: props.fotoPerfil
+    }
+    props.setDados(globalObjetc)
     props.setPageFlow(2);
-  };
+  }
+
+  function onChangeNome(event) {
+    props.setNome(event.target.value);
+  }
+
+  function onChangeFotoPerfil(event) {
+    props.setFotoPerfil(event.target.value);
+  }
+
+
+  
   return (
     <FormContainer>
       <Form>
         <StyledLabel>
           Nome:
-          <Input type={"text"} />
+          <Input type={"text"} onChange={onChangeNome} value={props.nome} /> {/* */}
         </StyledLabel>
         <StyledLabel>
           Foto de Perfil
-          <Input type={"text"} />
+          <Input type={"text"} onChange={onChangeFotoPerfil} value={props.fotoPerfil}/> {/**/}
         </StyledLabel>
         <SendButton onClick={login}>Fazer Login</SendButton>
       </Form>
--- src/components/FormularioPostagem/FormularioPostagem.js
+++ src/components/FormularioPostagem/FormularioPostagem.js
@@ -1,22 +1,48 @@
 import { FormContainer, Form, Input, StyledLabel, SendButton } from "./styled";
 
-export const FormularioCadastro = () => {
+export const FormularioCadastro = (props) => {
+
+  function enviar(){
+    const globalObjetcPostagem = {
+      titulo: props.titulo,
+      imagem: props.imagemPostagem, 
+      descricao: props.descricao
+    }
+    props.setDadosPostagem(globalObjetcPostagem)
+    props.setPageFlow(3);
+  }
+
+  function onChangeTitulo(event) {
+    props.setTitulo(event.target.value);
+  }
+
+  function onChangeImagemPostagem(event) {
+    props.setImagemPostagem(event.target.value);
+  }
+
+  function onChangeDescricao(event) {
+    props.setDescricao(event.target.value);
+  }
+
+
+
   return (
     <FormContainer>
       <h2>Insira sua postagem abaixo: </h2>
       <Form>
-        <StyledLabel htmlFor="titulo">
+        <StyledLabel htmlFor="titulo" >
           Titulo:
-          <Input id="titulo" />
+          <Input id="titulo" onChange = {onChangeTitulo} value = {props.titulo}/>
         </StyledLabel>
         <StyledLabel htmlFor="foto">
           Imagem:
-          <Input id="foto" />
+          <Input id="foto" type={"text"} onChange = {onChangeImagemPostagem} value = {props.imagemPostagem}/>
         </StyledLabel>
         <StyledLabel htmlFor="descricao">
           Descrição:
-          <Input id="descricao" />
+          <Input id="descricao"  onChange = {onChangeDescricao} value = {props.descricao}/>
         </StyledLabel>
+        <SendButton onClick={enviar}>Enviar</SendButton>  {/*onClick={} */}
       </Form>
     </FormContainer>
   );
